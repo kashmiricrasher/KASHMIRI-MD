5cb1cdd4347db600d5eec69d9c8b4efdba602e5a
KASHMIRI CRASHER <kashmirix3@gmail.com>
2025-04-15 13:28:43 +0500

    Add files via upload

---
+++ plugins/view.js
@@ -0,0 +1,246 @@
+function _0x402e49(_0x42d3cf, _0x252074, _0x4d1790, _0x51e21d) {
+  return _0x3a65(_0x51e21d + 0x35, _0x42d3cf);
+}
+(function (_0x1f136f, _0x7a563c) {
+  const _0x31f2a2 = _0x1f136f();
+  while (true) {
+    try {
+      const _0x1de00c = parseInt(_0x3a65(547, 0x1ec)) / 1 * (parseInt(_0x3a65(509, 0x438)) / 2) + -parseInt(_0x3a65(545, 0x461)) / 3 * (-parseInt(_0x3a65(563, 0x21f)) / 4) + parseInt(_0x3a65(479, 0x1a5)) / 5 + -parseInt(_0x3a65(566, 0x22d)) / 6 + -parseInt(_0x3a65(522, 0x43c)) / 7 + -parseInt(_0x3a65(577, 0x445)) / 8 + parseInt(_0x3a65(506, 0x41f)) / 9;
+      if (_0x1de00c === _0x7a563c) {
+        break;
+      } else {
+        _0x31f2a2.push(_0x31f2a2.shift());
+      }
+    } catch (_0x435279) {
+      _0x31f2a2.push(_0x31f2a2.shift());
+    }
+  }
+})(_0x32b1, 682729);
+const _0x3c6c20 = function () {
+  let _0x957105 = true;
+  return function (_0x3e9978, _0x29ff2a) {
+    const _0x4dfaf2 = _0x957105 ? function () {
+      if (_0x29ff2a) {
+        const _0x5f05db = _0x29ff2a.apply(_0x3e9978, arguments);
+        _0x29ff2a = null;
+        return _0x5f05db;
+      }
+    } : function () {};
+    _0x957105 = false;
+    return _0x4dfaf2;
+  };
+}();
+const _0x4ba0c5 = _0x3c6c20(this, function () {
+  return _0x4ba0c5.toString().search("(((.+)+)+)+$").toString().constructor(_0x4ba0c5).search("(((.+)+)+)+$");
+});
+function _0x3a65(_0x3baae8, _0x12077c) {
+  const _0x1736d3 = _0x32b1();
+  _0x3a65 = function (_0x4190a7, _0x10577a) {
+    _0x4190a7 = _0x4190a7 - 477;
+    let _0x1a822c = _0x1736d3[_0x4190a7];
+    if (_0x3a65.DalgOd === undefined) {
+      var _0x431ff8 = function (_0x1c4ec4) {
+        let _0x41574f = '';
+        let _0x3248dd = '';
+        let _0x34f8b4 = _0x41574f + _0x431ff8;
+        let _0x45d95b = 0;
+        let _0x5cf491;
+        let _0xa18199;
+        for (let _0x2d3c01 = 0; _0xa18199 = _0x1c4ec4.charAt(_0x2d3c01++); ~_0xa18199 && (_0x5cf491 = _0x45d95b % 4 ? _0x5cf491 * 64 + _0xa18199 : _0xa18199, _0x45d95b++ % 4) ? _0x41574f += _0x34f8b4.charCodeAt(_0x2d3c01 + 10) - 10 !== 0 ? String.fromCharCode(255 & _0x5cf491 >> (-2 * _0x45d95b & 6)) : _0x45d95b : 0) {
+          _0xa18199 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/='.indexOf(_0xa18199);
+        }
+        let _0x5be1fe = 0;
+        for (let _0x1610ef = _0x41574f.length; _0x5be1fe < _0x1610ef; _0x5be1fe++) {
+          _0x3248dd += '%' + ('00' + _0x41574f.charCodeAt(_0x5be1fe).toString(16)).slice(-2);
+        }
+        return decodeURIComponent(_0x3248dd);
+      };
+      _0x3a65.pbifVT = _0x431ff8;
+      _0x3baae8 = arguments;
+      _0x3a65.DalgOd = true;
+    }
+    const _0x28d727 = _0x1736d3[0];
+    const _0x680c57 = _0x4190a7 + _0x28d727;
+    const _0x6ee3 = _0x3baae8[_0x680c57];
+    if (!_0x6ee3) {
+      const _0x336198 = function (_0x5c4091) {
+        this.KKHCDh = _0x5c4091;
+        this.NRvUTP = [1, 0, 0];
+        this.UmiGdU = function () {
+          return 'newState';
+        };
+        this.OmSNCZ = "\\w+ *\\(\\) *{\\w+ *";
+        this.SmZtVv = "['|\"].+['|\"];? *}";
+      };
+      _0x336198.prototype.LhqDVY = function () {
+        const _0x53978e = new RegExp(this.OmSNCZ + this.SmZtVv);
+        const _0x5bf485 = _0x53978e.test(this.UmiGdU.toString()) ? --this.NRvUTP[1] : --this.NRvUTP[0];
+        return this.JPQYds(_0x5bf485);
+      };
+      _0x336198.prototype.JPQYds = function (_0x1abb8a) {
+        if (!Boolean(~_0x1abb8a)) {
+          return _0x1abb8a;
+        }
+        return this.bwtWtr(this.KKHCDh);
+      };
+      _0x336198.prototype.bwtWtr = function (_0xd33f6b) {
+        let _0x548ad9 = 0;
+        for (let _0x549dc4 = this.NRvUTP.length; _0x548ad9 < _0x549dc4; _0x548ad9++) {
+          this.NRvUTP.push(Math.round(Math.random()));
+          _0x549dc4 = this.NRvUTP.length;
+        }
+        return _0xd33f6b(this.NRvUTP[0]);
+      };
+      new _0x336198(_0x3a65).LhqDVY();
+      _0x1a822c = _0x3a65.pbifVT(_0x1a822c);
+      _0x3baae8[_0x680c57] = _0x1a822c;
+    } else {
+      _0x1a822c = _0x6ee3;
+    }
+    return _0x1a822c;
+  };
+  return _0x3a65(_0x3baae8, _0x12077c);
+}
+function _0x32b1() {
+  const _0x18c44a = ['x19WCM90B19F', 'ywXPyxm', 'BMn0Aw9UkcKG', 'BfbVDhO', 'yxvKAw8VBxa0', 'y29UC29Szq', 'yxvKAw8', 'mtq2mdyXrNfdsNnM', 'Aw1Hz2u', 'mJu3mZi2ruLmAhzt', 'Aw1Hz2vnzxnZyq', 'y3vyrMG', 'zxHJzxb0Aw9U', 'DhjHy2u', 'wM1js0i', 'y3rVCIGICMv0Dq', 'yMLUza', 'z2DcAe4', 'A2jurLy', 'zxnZywDLigjHyW', 'CM4GDgHPCYiPka', 'D1beAwS', 'CMv0DxjUicHMDq', 'u3b3D0C', 'y29UC3rYDwn0BW', 'mtaWu05fywPd', 'zMLSzw5HBwu', '4BshC3pHTidjOUg0HY4Q', 'ntq4mdi4nNHSC2LRqG', 'Dgv4De1LC3nHzW', 'Bxr5Cgu', 'Bg9N', 'ChPyrfe', 'rgzPv2W', 'C2fNzq', 'CxvVDgvK', 'y2fWDgLVBG', 'u1rWwue', 'zg93BMXVywq', 'mtaXmJiZmJbMrxDqtey', 'kcGOlISPkYKRkq', 'Dg9tDhjPBMC', 'B3DUzxi', 'zhnQEuC', 't3DUzxiGt25SEq', 'A0Pcq00', 'Dgv4Da', 'otmWnZCWuNnKrNHx', 'C2vHCMnO', 'ELPYtu0', 'zgvZyW', 'uwzYt1C', 'wwTQBM0', 'AhPLEKO', 'DMLKzw8', 'zNr1A2e', 'ChjVDg90ExbL', '4BseYPZjQSM0YAiG4BsBYPZHTiCG', 'C3rPy2TLCG', 'DwP5DMC', 'C2fNztOk', 'kVcFK5SG4BsaYBqG4BshYOdkGog0JW', 'C2vUzg1L', 'v2rjDxy', 'vKXvDeC', 'zg9JDw1LBNrnzq', 'C3rPy2TLCK1LCW', 'zg9JDw1LBNq', 'yxbWBhK', 'E30Uy29UC3rYDq', '4BsGYARHTiFHTkeG4BspYBtHTitHTiCG', 'C3nHz2u', 'zMLSzu5HBwu', 'AYb0BYb1C2vY', 'odaZnJy4nuvVrNrWBq', 'lI4Vy29UzMLN', 'lI4Vy29TBwfUza', 'mtbpuuPtruO', 's3DKyxa', '4Bsn4BshC3pHTidjOUg0HYeQ', 'weD1Afy', 'yxbWBgLJyxrPBW', '4BsGYARHTiFHTkhHTi/jTog0Hog0HYdHTi0', 'D2fYBG', '4P2mievYCM9YihzP', 'yxvKAw9nzxnZyq', 'y2f0zwDVCNK', 'C2vUze1LC3nHzW', 'AwDYug0', 'r3PlD3a', 'nta1mdy2ogrtA3f1uG', 'zxjYB3i', 'DK92vNO', 'C05lrgm', 'vxbcz3O', 'tM5Wyue', 'BhPdAwK', 'CM9YoG', 'rLvRu2C', 'Aw1Hz2uVANbLzW', 'zxDVBMnLig1LCW', 'B0fIDNi', 'kVcFJyhHTjJkN+g0H+g0GhpHTiCGYOa', 'iog0OCQCYARkN+g0HYdsK+g0H+g0MW', 'uhL5CxO', 'BwLTzxr5Cgu'];
+  _0x32b1 = function () {
+    return _0x18c44a;
+  };
+  return _0x32b1();
+}
+_0x4ba0c5();
+const _0x225284 = function () {
+  let _0xbc43f5 = true;
+  return function (_0x26f668, _0x44bee6) {
+    const _0x8a46e5 = _0xbc43f5 ? function () {
+      if (_0x44bee6) {
+        const _0x4723ae = _0x44bee6.apply(_0x26f668, arguments);
+        _0x44bee6 = null;
+        return _0x4723ae;
+      }
+    } : function () {};
+    _0xbc43f5 = false;
+    return _0x8a46e5;
+  };
+}();
+const _0x3b0ec0 = _0x225284(this, function () {
+  const _0x4f57dc = function () {
+    let _0x2b85ec;
+    try {
+      _0x2b85ec = Function("return (function() {}.constructor(\"return this\")( ));")();
+    } catch (_0x5cbd3b) {
+      _0x2b85ec = window;
+    }
+    return _0x2b85ec;
+  };
+  const _0x2347a9 = _0x4f57dc();
+  const _0x557e8f = _0x2347a9.console = _0x2347a9.console || {};
+  const _0x3d34cd = ["log", "warn", 'info', "error", "exception", 'table', "trace"];
+  for (let _0x123a79 = 0; _0x123a79 < _0x3d34cd.length; _0x123a79++) {
+    const _0x4484b2 = _0x225284.constructor.prototype.bind(_0x225284);
+    const _0x4afe49 = _0x3d34cd[_0x123a79];
+    const _0x214720 = _0x557e8f[_0x4afe49] || _0x4484b2;
+    _0x4484b2.__proto__ = _0x225284.bind(_0x225284);
+    _0x4484b2.toString = _0x214720.toString.bind(_0x214720);
+    _0x557e8f[_0x4afe49] = _0x4484b2;
+  }
+});
+function _0x2371ea(_0x53f20c, _0x2b73fe, _0x5e176c, _0x5a2676) {
+  return _0x3a65(_0x5e176c - 0x21b, _0x2b73fe);
+}
+_0x3b0ec0();
+const {
+  cmd
+} = require("../command");
+const _0x18d407 = {
+  pattern: 'v',
+  alias: ["sendme", 'vv'],
+  react: '🎐',
+  desc: "Owner Only - Forwards quoted message back to user",
+  category: "owner",
+  filename: __filename
+};
+cmd(_0x18d407, async (_0x568c05, _0x10cf5e, _0x561760, {
+  from: _0x56d37c,
+  quoted: _0x595ae4,
+  reply: _0x159feb,
+  pushname: _0x2ac8c4
+}) => {
+  try {
+    if (!_0x595ae4) {
+      return _0x159feb("*🍁ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴠɪᴇᴡ ᴏɴᴄᴇ ᴍᴇssᴀɢᴇ!*");
+    }
+    const _0x4c4342 = await _0x561760.quoted.download();
+    const _0x4f6273 = _0x561760.quoted.mtype;
+    const _0x38a0e2 = {
+      quoted: _0x10cf5e
+    };
+    let _0x308f68 = {};
+    switch (_0x4f6273) {
+      case "imageMessage":
+        const _0x467358 = {
+          image: _0x4c4342,
+          caption: _0x561760.quoted.text || '',
+          mimetype: _0x561760.quoted.mimetype || "image/jpeg"
+        };
+        _0x308f68 = _0x467358;
+        break;
+      case "videoMessage":
+        const _0x2fb875 = {
+          video: _0x4c4342,
+          caption: _0x561760.quoted.text || '',
+          mimetype: _0x561760.quoted.mimetype || 'video/mp4'
+        };
+        _0x308f68 = _0x2fb875;
+        break;
+      case "audioMessage":
+        const _0x3a049c = {
+          audio: _0x4c4342,
+          mimetype: "audio/mp4",
+          ptt: _0x561760.quoted.ptt || false
+        };
+        _0x308f68 = _0x3a049c;
+        break;
+      case "stickerMessage":
+        const _0x2b9e82 = {
+          sticker: _0x4c4342
+        };
+        _0x308f68 = _0x2b9e82;
+        break;
+      case "documentMessage":
+        const _0x540b63 = {
+          document: _0x4c4342,
+          mimetype: _0x561760.quoted.mimetype || "application/octet-stream",
+          fileName: _0x561760.quoted.fileName || 'document'
+        };
+        _0x308f68 = _0x540b63;
+        break;
+      case "textMessage":
+      default:
+        if (_0x561760.quoted.text) {
+          const _0x58dc1b = {
+            text: _0x561760.quoted.text
+          };
+          _0x308f68 = _0x58dc1b;
+        } else {
+          const _0x4048b8 = {
+            'text': "*📛 ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ғᴇᴛᴄʜɪɴɢ ᴛʜᴇ ᴠɪᴇᴡᴏɴᴄᴇ ᴍᴇssᴀɢᴇ.*"
+          };
+          const _0x279e33 = {
+            quoted: _0x10cf5e
+          };
+          return await _0x568c05.sendMessage(_0x56d37c, _0x4048b8, _0x279e33);
+        }
+    }
+    await _0x568c05.sendMessage(_0x56d37c, _0x308f68, _0x38a0e2);
+  } catch (_0x223635) {
+    console.error("Forward Error:", _0x223635);
+    const _0x56ee4c = {
+      text: "❌ Error viewonce message:\n" + _0x223635.message
+    };
+    const _0x5c5406 = {
+      quoted: _0x10cf5e
+    };
+    await _0x568c05.sendMessage(_0x56d37c, _0x56ee4c, _0x5c5406);
+  }
+});
---
+++ plugins/ytdl3.js
@@ -0,0 +1,367 @@
+(function (_0x25be3e, _0x3401ac) {
+  const _0x36e597 = _0x25be3e();
+  while (true) {
+    try {
+      const _0x2c83b2 = parseInt(_0x18e7(399, 0x52d)) / 1 * (parseInt(_0x18e7(275, -0x234)) / 2) + parseInt(_0x18e7(314, -0x1da)) / 3 * (-parseInt(_0x18e7(418, -0x15b)) / 4) + -parseInt(_0x18e7(385, 0x53a)) / 5 * (parseInt(_0x18e7(411, 0x547)) / 6) + -parseInt(_0x18e7(480, 0x539)) / 7 * (parseInt(_0x18e7(391, -0x18e)) / 8) + parseInt(_0x18e7(459, 0x4f8)) / 9 * (parseInt(_0x18e7(341, 0x50b)) / 10) + parseInt(_0x18e7(369, -0x158)) / 11 * (-parseInt(_0x18e7(463, 0x4cf)) / 12) + parseInt(_0x18e7(299, 0x457)) / 13;
+      if (_0x2c83b2 === _0x3401ac) {
+        break;
+      } else {
+        _0x36e597.push(_0x36e597.shift());
+      }
+    } catch (_0x20b939) {
+      _0x36e597.push(_0x36e597.shift());
+    }
+  }
+})(_0x144f, 466876);
+function _0x58865b(_0x3364f1, _0x9bbd9e, _0x4b4acc, _0x2e8173) {
+  return _0x18e7(_0x2e8173 - 0x18c, _0x3364f1);
+}
+const _0x718381 = function () {
+  let _0x4cdb9c = true;
+  return function (_0x160fa9, _0x543e65) {
+    const _0x15964d = _0x4cdb9c ? function () {
+      if (_0x543e65) {
+        const _0x5878ea = _0x543e65.apply(_0x160fa9, arguments);
+        _0x543e65 = null;
+        return _0x5878ea;
+      }
+    } : function () {};
+    _0x4cdb9c = false;
+    return _0x15964d;
+  };
+}();
+const _0x26e114 = _0x718381(this, function () {
+  return _0x26e114.toString().search("(((.+)+)+)+$").toString().constructor(_0x26e114).search("(((.+)+)+)+$");
+});
+_0x26e114();
+const _0x367946 = function () {
+  const _0x8486b0 = {
+    hPZHw: function (_0x1842b8, _0x1b511f) {
+      return _0x1842b8 !== _0x1b511f;
+    }
+  };
+  _0x8486b0.OQuUy = 'vabnx';
+  let _0x571b54 = true;
+  return function (_0x56170a, _0x4eec16) {
+    const _0x277c6e = {
+      'femCS': function (_0x101c5c, _0x393162) {
+        return _0x101c5c !== _0x393162;
+      },
+      'PKGBa': _0x8486b0.OQuUy
+    };
+    const _0x4b2836 = _0x571b54 ? function () {
+      if ("ACfym" !== _0x277c6e.PKGBa) {
+        if (_0x4eec16) {
+          const _0x178dde = _0x4eec16.apply(_0x56170a, arguments);
+          _0x4eec16 = null;
+          return _0x178dde;
+        }
+      } else {
+        if (_0x4f35db) {
+          const _0xd9d3d1 = _0x49315d.apply(_0x567641, arguments);
+          _0x3ab00 = null;
+          return _0xd9d3d1;
+        }
+      }
+    } : function () {};
+    _0x571b54 = false;
+    return _0x4b2836;
+  };
+}();
+const _0x2c55ff = _0x367946(this, function () {
+  const _0xf9f1ea = function () {
+    let _0x3fbedb;
+    try {
+      _0x3fbedb = Function("return (function() {}.constructor(\"return this\")( ));")();
+    } catch (_0xf9ee65) {
+      _0x3fbedb = window;
+    }
+    return _0x3fbedb;
+  };
+  const _0x246d62 = _0xf9f1ea();
+  const _0x23b87f = _0x246d62.console = _0x246d62.console || {};
+  const _0x2f45b7 = ["log", "warn", "info", "error", 'exception', 'table', "trace"];
+  for (let _0x2846eb = 0; _0x2846eb < _0x2f45b7.length; _0x2846eb++) {
+    const _0x3156c4 = _0x367946.constructor.prototype.bind(_0x367946);
+    const _0x29721c = _0x2f45b7[_0x2846eb];
+    const _0x51d22c = _0x23b87f[_0x29721c] || _0x3156c4;
+    _0x3156c4.__proto__ = _0x367946.bind(_0x367946);
+    _0x3156c4.toString = _0x51d22c.toString.bind(_0x51d22c);
+    _0x23b87f[_0x29721c] = _0x3156c4;
+  }
+});
+_0x2c55ff();
+function _0x144f() {
+  const _0x182506 = ['1203633183', 'download_u', '7XorMwj', 'xadMY', 'bind', 'filename', 'forwarding', 'zRFEW', "NOTE* 🎙️\n\n*", 'result', 'MxpEO', 'Name', "*_oops No ", 'WENLC', '{}.constru', 'WpBvy', 'rAEif', 'author', 'length', 'category', "𝐏ʀ๏νιɖє 𝐀 ", "ғꪮʀ ʏꪮꪊ ғꪮ", 'key', "eply with ", 'zKdvv', "3) ❤️", 'trim', 'ySVmm', '40uTeniF', "An error o", 'AVxAD', 'rHSHZ', "ɖє๏ 𝐍αмє..", 'ipZZT', 'rl=', 'vMXRw', " 𝐕𝐈𝐃𝐄𝐎 𝐃-𝐋", "video. Ple", 'LIDMd', 'SoTnL', 'TOUSw', 'main', 'console', 'aaNBi', 'ubqBp', 'is.davidcy', 'Jid', 'json', " choice! R", 'sendMessag', 'sMAgt', 'OQuUy', '20614347MWMEJf', 'prototype', "ꜱ 🍉*", 'fsWWr', 'mentionedJ', ". ⌬ VIDEO ", "\n👀 *VIEWS:", 'caption', '@dark-yasi', 'apply', "𝐌Ɗ 𝐕𝐈𝐃𝐄𝐎 𝐃", 'https://ap', '.mp3', 'PKGBa', ". ⌬ AUDIO ", '1541433vZyhZU', "Download Y", 'serverMess', 'WMmzJ', "1 , 2 , 3*", 'cOQSl', 'GdrTf', "𝐒๏ƞ͛g 𝐍αмє.", 'title', 'cMplX', 'results', "ase try ag", 'RvBMA', "\n⏰ *DURATI", 'Score', 'messages.u', ".mp4 < Yt ", " found!", '(((.+)+)+)', 'downloadUr', 'PnXIi', 'isForwarde', "ain later.", "ply with (", 'ad/ytmp4?u', "*Please Re", 'quoted', '10ddtihz', '𓆪͛', 'uHCew', 'msBmv', "again late", 'JgntI', 'search', "\n\n*🍓 REPLY", " 1 , 2 or ", "TYPE* 🎧\n*3", 'gpECl', "return (fu", 'wlQoY', 'VrNlF', 'message', "Failed to ", 'image', " ۫*\n🎡 *TIT", 'hcOYy', 'riltech.my', 'mimetype', 'PjRLP', "๏νιɖє 𝐀 𝐘ʈ", 'psert', "*⛔ Invalid", 'ElzGp', 'extendedTe', 'document', '11IFzSwU', 'error', 'success', 'ྀི𓆪͛', 'femCS', "*1. ⌬ DOCU", 'views', 'contextInf', 'yta', 'newsletter', 'hPZHw', 'desc', 'mp4', "*𝐏ℓєα𝐬֟፝є ", '../command', "nction() ", '432865oFcmXc', ". ⌬ VOICE ", 'rWxph', 'vJWGR', 'Wruap', 'ACfym', '2703128IJzFWu', 'trace', 'VeHHb', '87454868@n', "-𝐋 🪾͎᪳᪳𝆺𝅥 ⃙̊- ִ", 'uOPfU', 'OKTnZ', ":* ", '32106zWrWkj', "ouTube vid", 'react', "*\n🎡 *TITLE", " WITH YOUR", "\n👤 *AUTHOR", "ctor(\"retu", 'ageId', 'oZDCW', 'YKfnB', "results fo", " 🪾͎᪳᪳𝆺𝅥 ⃙̊- ִ ۫", '60pujTcY', "lease try ", " CHOICE:*\n", 'audio/mpeg', "url or Nam", 'dXtUT', 'hYeVN', '4KwDOcE', 'video', 'dMICo', "MENT* 📑\n*2", 'pattern', "TYPE* 🎥\n\n*", 'gtXgh', 'alias', '.mp4', "ccurred. P", 'audio', 'JutIB', 'yZCLK', 'forwardedN', 'xtMessage', "audio. Ple", 'fileName', "𓆩ྀི͛ᴋᴀsʜᴍɪʀɪ 𝐌Ɗ", 'ZBdIW', 'QpUpU', "*𝐏ℓєαʂє 𝐏ɼ", 'constructo', "ON:* ", "fetch the ", "*͎᪳᪳🫟ᩧ𝅄ᴋᴀsʜᴍɪʀɪ ", "ouTube son", 'DcIoq', 'essageInfo', 'name', 'url', 'fLfDI', 'sender', " 𝐏𝐋𝐀𝐘𝐈𝐍𝐆ྀི", 'XrPTN', "e >", ".song < Yt", 'song', 'vCGVT', 'ewsletter', 'wHkYl', 'stanzaId', '6691905lwAgNo', 'log', 'status', 'video/mp4', '9444912UzuiZG', 'hOFeO', 'warn', 'tRIEG', 'toString', 'text', 'Wyhbl', 'FlJhy', 'JTKIl', 'timestamp', 'IwmFU', "ʀ ᴀʟʟ ꪮғ ᴀ", 'info', 'rcyjA', 'ptt'];
+  _0x144f = function () {
+    return _0x182506;
+  };
+  return _0x144f();
+}
+const {
+  cmd
+} = require("../command");
+const {
+  ytsearch,
+  ytmp3,
+  ytmp4
+} = require("@dark-yasiya/yt-dl.js");
+const _0xe9f79b = {
+  pattern: "mp4",
+  alias: ["video", 'ytv'],
+  react: '🎥',
+  desc: "Download YouTube video",
+  category: "main",
+  use: ".mp4 < Yt url or Name >",
+  filename: __filename
+};
+cmd(_0xe9f79b, async (_0x255bfd, _0x10899e, _0x319f07, {
+  from: _0x4827de,
+  prefix: _0x32c149,
+  quoted: _0x2ae5a3,
+  q: _0x415833,
+  reply: _0x54b7d4
+}) => {
+  try {
+    if (!_0x415833) {
+      return await _0x54b7d4("*𝐏ℓєαʂє 𝐏ɼ๏νιɖє 𝐀 𝐘ʈ 𝐔ɼℓ ๏ɼ 𝐕ιɖє๏ 𝐍αмє..*");
+    }
+    const _0x5a2393 = await ytsearch(_0x415833);
+    if (_0x5a2393.results.length < 1) {
+      return _0x54b7d4("No results found!");
+    }
+    let _0x5e0af8 = _0x5a2393.results[0];
+    let _0x566827 = "https://apis.davidcyriltech.my.id/download/ytmp4?url=" + encodeURIComponent(_0x5e0af8.url);
+    let _0x5488e7 = await fetch(_0x566827);
+    let _0xf7570a = await _0x5488e7.json();
+    if (_0xf7570a.status !== 200 || !_0xf7570a.success || !_0xf7570a.result.download_url) {
+      return _0x54b7d4("Failed to fetch the video. Please try again later.");
+    }
+    let _0x431d6a = "*͎᪳᪳🫟ᩧ𝅄ᴋᴀsʜᴍɪʀɪ 𝐌Ɗ 𝐕𝐈𝐃𝐄𝐎 𝐃-𝐋 🪾͎᪳᪳𝆺𝅥 ⃙̊- ִ ۫*\n🎡 *TITLE:* " + _0x5e0af8.title + "\n⏰ *DURATION:* " + _0x5e0af8.timestamp + "\n👀 *VIEWS:* " + _0x5e0af8.views + "\n👤 *AUTHOR:* " + _0x5e0af8.author.name + "\n\n*🍓 REPLY WITH YOUR CHOICE:*\n*1. ⌬ DOCUMENT* 📑\n*2. ⌬ VIDEO TYPE* 🎥\n\n*ғꪮʀ ʏꪮꪊ ғꪮʀ ᴀʟʟ ꪮғ ᴀꜱ 🍉*";
+    const _0x39532b = {
+      newsletterJid: "120363312991035785@newsletter",
+      newsletterName: "𓆩ྀི͛ᴋᴀsʜᴍɪʀɪ 𝐌Ɗ 𝐕𝐈𝐃𝐄𝐎 𝐃-𝐋ྀི𓆪͛",
+      serverMessageId: 0x8f
+    };
+    const _0x2f4033 = {
+      mentionedJid: [_0x319f07.sender],
+      forwardingScore: 0x3e7,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: _0x39532b
+    };
+    const _0x11d3a6 = {
+      url: _0x5e0af8.thumbnail
+    };
+    const _0xf8e957 = {
+      image: _0x11d3a6,
+      caption: _0x431d6a,
+      contextInfo: _0x2f4033
+    };
+    const _0x54804e = {
+      quoted: _0x10899e
+    };
+    const _0x6a69a1 = await _0x255bfd.sendMessage(_0x4827de, _0xf8e957, _0x54804e);
+    _0x255bfd.ev.on("messages.upsert", async _0x5b7404 => {
+      const _0x3d1e54 = _0x5b7404.messages[0];
+      if (!_0x3d1e54.message || !_0x3d1e54.message.extendedTextMessage) {
+        return;
+      }
+      const _0x5bee72 = _0x3d1e54.message.extendedTextMessage.text.trim();
+      if (_0x3d1e54.message.extendedTextMessage.contextInfo && _0x3d1e54.message.extendedTextMessage.contextInfo.stanzaId === _0x6a69a1.key.id) {
+        const _0x24afed = {
+          text: '⬇️',
+          key: _0x3d1e54.key
+        };
+        const _0x55c7a3 = {
+          react: _0x24afed
+        };
+        await _0x255bfd.sendMessage(_0x4827de, _0x55c7a3);
+        switch (_0x5bee72) {
+          case '1':
+            const _0x5dbc2c = {
+              url: _0xf7570a.result.download_url
+            };
+            const _0x3e9560 = {
+              document: _0x5dbc2c,
+              mimetype: "video/mp4",
+              fileName: _0x5e0af8.title + ".mp4",
+              contextInfo: _0x2f4033
+            };
+            const _0x1aebbd = {
+              quoted: _0x3d1e54
+            };
+            await _0x255bfd.sendMessage(_0x4827de, _0x3e9560, _0x1aebbd);
+            break;
+          case '2':
+            const _0x37681a = {
+              'url': _0xf7570a.result.download_url
+            };
+            const _0x53ad18 = {
+              video: _0x37681a,
+              mimetype: "video/mp4",
+              contextInfo: _0x2f4033
+            };
+            const _0xe7540 = {
+              quoted: _0x3d1e54
+            };
+            await _0x255bfd.sendMessage(_0x4827de, _0x53ad18, _0xe7540);
+            break;
+          default:
+            const _0x282043 = {
+              text: "*Please Reply with ( 1 , 2 or 3) ❤️"
+            };
+            const _0x1328d9 = {
+              quoted: _0x3d1e54
+            };
+            await _0x255bfd.sendMessage(_0x4827de, _0x282043, _0x1328d9);
+            break;
+        }
+      }
+    });
+  } catch (_0x4ebad8) {
+    console.log(_0x4ebad8);
+    _0x54b7d4("An error occurred. Please try again later.");
+  }
+});
+const _0x58c311 = {};
+function _0x18e7(_0x17ef7a, _0x26e114) {
+  const _0x718381 = _0x144f();
+  _0x18e7 = function (_0x144f87, _0x18e769) {
+    _0x144f87 = _0x144f87 - 269;
+    let _0x36fa18 = _0x718381[_0x144f87];
+    return _0x36fa18;
+  };
+  return _0x18e7(_0x17ef7a, _0x26e114);
+}
+_0x58c311.pattern = "yta";
+_0x58c311.alias = ["audio", "song"];
+_0x58c311.react = '🎶';
+_0x58c311.desc = "Download YouTube song";
+_0x58c311.category = "main";
+_0x58c311.use = ".song < Yt url or Name >";
+_0x58c311.filename = __filename;
+function _0x1f4827(_0x209f89, _0x49c2b3, _0x2da24e, _0x53d00c) {
+  return _0x18e7(_0x2da24e - 0x19a, _0x49c2b3);
+}
+cmd(_0x58c311, async (_0x21382d, _0x9f56f, _0x2a1cd2, {
+  from: _0x5d25e8,
+  prefix: _0x3b8418,
+  quoted: _0x5e6aa2,
+  q: _0x3e5213,
+  reply: _0x3b567c
+}) => {
+  try {
+    if (!_0x3e5213) {
+      return await _0x3b567c("*𝐏ℓєα𝐬֟፝є 𝐏ʀ๏νιɖє 𝐀 𝐒๏ƞ͛g 𝐍αмє..*");
+    }
+    const _0x34bb4b = await ytsearch(_0x3e5213);
+    if (_0x34bb4b.results.length < 1) {
+      return _0x3b567c("*_oops No results found!_*");
+    }
+    let _0x124ee8 = _0x34bb4b.results[0];
+    let _0x54aa03 = "https://apis.davidcyriltech.my.id/youtube/mp3?url=" + encodeURIComponent(_0x124ee8.url);
+    let _0x34036b = await fetch(_0x54aa03);
+    let _0x13cdbe = await _0x34036b.json();
+    if (_0x13cdbe.status !== 200 || !_0x13cdbe.success || !_0x13cdbe.result.downloadUrl) {
+      return _0x3b567c("Failed to fetch the audio. Please try again later.");
+    }
+    let _0x21589e = "*͎᪳᪳🫟ᩧ𝅄ᴋᴀsʜᴍɪʀɪ 𝐌Ɗ 𝐏𝐋𝐀𝐘𝐈𝐍𝐆 🪾͎᪳᪳𝆺𝅥 ⃙̊- ִ ۫*\n🎡 *TITLE:* " + _0x124ee8.title + "\n⏰ *DURATION:* " + _0x124ee8.timestamp + "\n👀 *VIEWS:* " + _0x124ee8.views + "\n👤 *AUTHOR:* " + _0x124ee8.author.name + "\n\n*🍓 REPLY WITH YOUR CHOICE:*\n*1. ⌬ DOCUMENT* 📑\n*2. ⌬ AUDIO TYPE* 🎧\n*3. ⌬ VOICE NOTE* 🎙️\n\n*ғꪮʀ ʏꪮꪊ ғꪮʀ ᴀʟʟ ꪮғ ᴀꜱ 🍉*";
+    const _0x185dba = {
+      newsletterJid: "120363312991035785@newsletter",
+      newsletterName: "𓆩ྀི͛ᴋᴀsʜᴍɪʀɪ 𝐌Ɗ 𝐏𝐋𝐀𝐘𝐈𝐍𝐆ྀི𓆪͛",
+      serverMessageId: 0x8f
+    };
+    const _0x46c8e3 = {
+      mentionedJid: [_0x2a1cd2.sender],
+      forwardingScore: 0x3e7,
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: _0x185dba
+    };
+    const _0x29aa78 = {
+      url: _0x124ee8.thumbnail
+    };
+    const _0x22908f = {
+      image: _0x29aa78,
+      caption: _0x21589e,
+      contextInfo: _0x46c8e3
+    };
+    const _0x3c48ff = {
+      'quoted': _0x9f56f
+    };
+    const _0xe1e0ae = await _0x21382d.sendMessage(_0x5d25e8, _0x22908f, _0x3c48ff);
+    _0x21382d.ev.on("messages.upsert", async _0x95b66a => {
+      const _0x242410 = _0x95b66a.messages[0];
+      if (!_0x242410.message || !_0x242410.message.extendedTextMessage) {
+        return;
+      }
+      const _0x15c2e5 = _0x242410.message.extendedTextMessage.text.trim();
+      if (_0x242410.message.extendedTextMessage.contextInfo && _0x242410.message.extendedTextMessage.contextInfo.stanzaId === _0xe1e0ae.key.id) {
+        const _0x556029 = {
+          text: '⏰',
+          key: _0x242410.key
+        };
+        const _0x4c3725 = {
+          react: _0x556029
+        };
+        await _0x21382d.sendMessage(_0x5d25e8, _0x4c3725);
+        switch (_0x15c2e5) {
+          case '1':
+            const _0x103599 = {
+              url: _0x13cdbe.result.downloadUrl
+            };
+            const _0x1ee605 = {
+              document: _0x103599,
+              mimetype: "audio/mpeg",
+              fileName: _0x124ee8.title + ".mp3",
+              contextInfo: _0x46c8e3
+            };
+            const _0x419679 = {
+              quoted: _0x242410
+            };
+            await _0x21382d.sendMessage(_0x5d25e8, _0x1ee605, _0x419679);
+            break;
+          case '2':
+            const _0x418d86 = {
+              url: _0x13cdbe.result.downloadUrl
+            };
+            const _0xd5ddc6 = {
+              audio: _0x418d86,
+              mimetype: "audio/mpeg",
+              contextInfo: _0x46c8e3
+            };
+            const _0x1337ef = {
+              quoted: _0x242410
+            };
+            await _0x21382d.sendMessage(_0x5d25e8, _0xd5ddc6, _0x1337ef);
+            break;
+          case '3':
+            const _0x1fab2a = {
+              url: _0x13cdbe.result.downloadUrl
+            };
+            const _0x5db9a2 = {
+              audio: _0x1fab2a,
+              mimetype: "audio/mpeg",
+              ptt: true,
+              contextInfo: _0x46c8e3
+            };
+            const _0x391b5a = {
+              quoted: _0x242410
+            };
+            await _0x21382d.sendMessage(_0x5d25e8, _0x5db9a2, _0x391b5a);
+            break;
+          default:
+            const _0x29a7c4 = {
+              'text': "*⛔ Invalid choice! Reply with 1 , 2 , 3*"
+            };
+            const _0x5a3271 = {
+              quoted: _0x242410
+            };
+            await _0x21382d.sendMessage(_0x5d25e8, _0x29a7c4, _0x5a3271);
+        }
+      }
+    });
+  } catch (_0xef1697) {
+    console.log(_0xef1697);
+    _0x3b567c("An error occurred. Please try again later.");
+  }
+});
